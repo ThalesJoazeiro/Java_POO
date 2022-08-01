bbc091369d5cc7c93acd45f4ba87cc1cebcb32e8
Thales Joazeiro <thalesjoazeiro@gmail.com>
2022-08-01 13:55:11 -0300

    Melhorando a Classe

--- Tabuada/src/CalculadoraTabuada.java
+++ Tabuada/src/CalculadoraTabuada.java
@@ -11,6 +11,7 @@ public class CalculadoraTabuada{
 
     public void calcular(int valor){
         setValor(valor);
+        System.out.println("Tabuada do: " + getValor());
         for (int i = 0; i <= 10; i++){
             System.out.print(i + " x " + this.getValor() + " = ");
             System.out.println(i * this.getValor());
