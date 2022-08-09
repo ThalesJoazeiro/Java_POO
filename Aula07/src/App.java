333ea4eb8a054cb557c7fea3c63408a27716d8e2
Thales Joazeiro <thalesjoazeiro@gmail.com>
2022-08-09 20:07:11 -0300

    Update App.java

--- Aula07/src/App.java
+++ Aula07/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Lutador l[] = new Lutador[3];
+        Lutador l[] = new Lutador[4];
 
         l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
 
